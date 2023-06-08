400904581babc2164868c63de052ce4743c7fe77
Sainath A <sai.avaduta@gmail.com>
2023-06-08 16:44:51 +0530

    Add setDistance for all protocols

--- src/main/java/org/traccar/handler/DistanceHandler.java
+++ src/main/java/org/traccar/handler/DistanceHandler.java
@@ -77,6 +77,7 @@ public class DistanceHandler extends BaseDataHandler {
             }
         }
         position.set(Position.KEY_DISTANCE, distance);
+        position.setDistance(distance);
         totalDistance = BigDecimal.valueOf(totalDistance + distance).setScale(2, RoundingMode.HALF_EVEN).doubleValue();
         position.set(Position.KEY_TOTAL_DISTANCE, totalDistance);
 
